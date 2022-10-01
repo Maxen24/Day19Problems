8779924473eb7754fe413268aa189e5b0034b72f
Maxen24 <mshubham242@gmail.com>
2022-10-01 18:59:00 +0530

    Added User Registration Day 19 Problem UC3

--- src/main/java/com/bridgelabz/UserRegistrationProblemUC1.java
+++ src/main/java/com/bridgelabz/UserRegistrationProblemUC1.java
@@ -11,13 +11,28 @@ public class UserRegistrationProblemUC1 {
     //UC-2 As a User need to enter a valid Last
     //Name - Last name starts with Cap and has minimum 3 characters
 
-    public boolean lastName(String lastname){
+    public boolean lastName(String lastname) {
         String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
         return lastname.matches(nameRegex);
+    }
+
+
+    //UC-3 As a User need to enter
+
+  //  a valid email//- E.g. abc.xyz@bl.co.in -
+  //  Email has 3 mandatory parts (abc, bl& co) and
+  //  2 optional (xyz & in) with precise @ and . positions
+    public boolean emailID(String emailID){
+        String emailRegex="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        return emailID.matches(emailRegex);
     }
 
     public static void main(String[] args) {
         UserRegistrationProblemUC1 myFirstName = new UserRegistrationProblemUC1();
         System.out.println(myFirstName.firstName("Tom"));// enter first name only 3 characters with first letter Cap
+        UserRegistrationProblemUC1 myLastName = new UserRegistrationProblemUC1();
+        System.out.println(myLastName.lastName("Ben"));
+        UserRegistrationProblemUC1 myEmailID=new UserRegistrationProblemUC1();
+        System.out.println(myEmailID.emailID("Max.123@gmail.com"));
     }
 }
--- src/test/java/UserRegistrationProblemUC1Test.java
+++ src/test/java/UserRegistrationProblemUC1Test.java
@@ -27,6 +27,9 @@ public class UserRegistrationProblemUC1Test {
         boolean returnResult = myFirstName.firstName("AJ");
         Assert.assertFalse(returnResult);
     }
+
+    //  UC-2 As a User need to enter a valid Last
+    //Name - Last name starts with Cap and has minimum 3 characters.
     @Test
     public void checking_for_last_Name_that_is_valid() {
         UserRegistrationProblemUC1 myLastName = new UserRegistrationProblemUC1();
@@ -47,4 +50,29 @@ public class UserRegistrationProblemUC1Test {
         boolean returnResult = myLastName.firstName("AJ");
         Assert.assertFalse(returnResult);
     }
+      //UC-3 As a User need to enter
+
+    //  a valid email//- E.g. abc.xyz@bl.co.in -
+    //  Email has 3 mandatory parts (abc, bl& co) and
+    //  2 optional (xyz & in) with precise @ and . positions
+    @Test
+    public void check_given_email_ID(){
+        UserRegistrationProblemUC1 myEmailID=new UserRegistrationProblemUC1();
+        boolean returnResult=myEmailID.emailID("Max.123@gmail.com");
+        Assert.assertTrue(returnResult);
+    }
+
+    @Test
+    public void checking_given_email_ID_by_changing_dot_position_return_false(){
+        UserRegistrationProblemUC1 myEmailID=new UserRegistrationProblemUC1();
+        boolean returnResult=myEmailID.emailID("gmail.com123.max@");
+        Assert.assertFalse(returnResult);
+    }
+    @Test
+    public void checking_given_email_ID_by_changing_special_character_position_return_false(){
+        UserRegistrationProblemUC1 myEmailID= new UserRegistrationProblemUC1();
+        boolean returnResult=myEmailID.emailID("gmailxyz123.com@");
+        Assert.assertFalse(returnResult);
+    }
 }
+
